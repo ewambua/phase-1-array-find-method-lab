124c788d2ef5af403b93558e4fc0f690b4d07ebe
EmmanuelWambua <emmanuelwambua931@gmail.com>
2023-03-17 23:33:50 +0300

    done with Lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+
+
+let foundItem = record.find(superbowlWin)
+
+
+function superbowlWin(record){
+    for(let item of record){
+        if(item.result === "W"){
+            return item.year;
+        }
+    }
+}
